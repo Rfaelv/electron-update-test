4dbcef079682493846fc744a6045e88df5bbd796
Rafael.V <ravieira@id.uff.br>
2021-04-13 10:33:27 -0300

    v4

--- main.js
+++ main.js
@@ -43,7 +43,16 @@ ipcMain.on('app_version', (event) => {
 
 autoUpdater.on('update-available', () => {
     mainWindow.webContents.send('update_available');
-  });
+});
+autoUpdater.on('update-not-available', () => {
+    mainWindow.webContents.send('update-not-available');
+});
+autoUpdater.on('error', () => {
+  mainWindow.webContents.send('error');
+});
+// autoUpdater.on('error', (err) => {
+//     sendStatusToWindow('Error in auto-updater. ' + err);
+// })
 autoUpdater.on('update-downloaded', () => {
     mainWindow.webContents.send('update_downloaded');
 });
